2b47ecaf3fef29b88659afe653bf6870b515ef21
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-05-01 16:11:35 +0200

    add section banner promo and solucion media query

---
+++ components/bannerPromo/banner.js
@@ -0,0 +1,24 @@
+import './style_banner_promo.css'
+
+//ARRAYS
+
+const banner = document.querySelector('#banner_promo')
+//CREATE
+const div = document.createElement('div')
+const a = document.createElement('a')
+const img = document.createElement('img')
+
+//ATTRIBUTE
+div.setAttribute('class', 'banner_promo')
+a.setAttribute('href', '#container_cards')
+img.setAttribute('src', '../../public/img/LevelUpSale.png')
+img.setAttribute('alt', 'banner promotional day')
+
+//TEXT CONTENT
+
+//APPEND
+banner.appendChild(div)
+div.appendChild(a)
+a.appendChild(img)
+
+//ARRAY FOR APPEND
