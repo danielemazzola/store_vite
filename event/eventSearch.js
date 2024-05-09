4c7dbfb011cc248c19d62cfd4b57af12a4538024
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-05-09 09:49:51 +0200

    Add no result alert

--- event/eventSearch.js
+++ event/eventSearch.js
@@ -6,7 +6,6 @@ const sectionSearch = document.createElement('section')
 //EVENTS
 input.addEventListener('change', (e) => {
   const INPUT = e.target.value
-  console.log(select.value)
   if (select.value === '') {
     const result = arrayProducts.filter((val) => val.price <= INPUT)
     returnResult(result)
@@ -41,60 +40,75 @@ const returnResult = (newArray) => {
   sectionSearch.appendChild(divResult)
   sectionSearch.appendChild(divFlex)
   divFlex.appendChild(divGrid)
-  for (const arrayProduct of newArray) {
-    //CREATE
-    const wordRecomended = document.createElement('p')
-    const divCard = document.createElement('div')
-    const divCardImg = document.createElement('div')
-    const seller = document.createElement('p')
-    const img = document.createElement('img')
-    const p = document.createElement('p')
-    const a = document.createElement('a')
-    const b = document.createElement('b')
-    const divDescriptionCard = document.createElement('div')
-    const hthree = document.createElement('h3')
-    const pDescription = document.createElement('p')
-    const spanDescription = document.createElement('span')
-    const btnCard = document.createElement('button')
+  if (newArray.length <= 0) {
+    const alert = document.createElement('p')
+    alert.setAttribute(
+      'style',
+      'text-align:center; margin-bottom:20px; color:red;'
+    )
+    alert.textContent =
+      'No hay resultados de busqueda, prueba a cambiar el importe.'
+    sectionSearch.append(alert)
+    setTimeout(() => {
+      sectionSearch.innerHTML = ``
+      sectionSearch.remove()
+    }, 3000)
+  } else {
+    for (const arrayProduct of newArray) {
+      //CREATE
+      const wordRecomended = document.createElement('p')
+      const divCard = document.createElement('div')
+      const divCardImg = document.createElement('div')
+      const seller = document.createElement('p')
+      const img = document.createElement('img')
+      const p = document.createElement('p')
+      const a = document.createElement('a')
+      const b = document.createElement('b')
+      const divDescriptionCard = document.createElement('div')
+      const hthree = document.createElement('h3')
+      const pDescription = document.createElement('p')
+      const spanDescription = document.createElement('span')
+      const btnCard = document.createElement('button')
 
-    //ATTRIBUTE
-    divCard.setAttribute('class', 'card_grid')
-    divCardImg.setAttribute('class', 'card_grid_img')
-    img.setAttribute('loading', 'lazy')
-    img.setAttribute('src', arrayProduct.image)
-    img.setAttribute('alt', arrayProduct.name)
-    seller.classList.add('seller')
-    p.setAttribute('class', 'card_item_p')
-    a.setAttribute('href', arrayProduct.href)
-    a.setAttribute('loading', 'lazy')
-    a.setAttribute('class', 'text_special_card')
-    divDescriptionCard.setAttribute('class', 'description_card spacing_top')
-    btnCard.setAttribute('class', 'btn-buy')
+      //ATTRIBUTE
+      divCard.setAttribute('class', 'card_grid')
+      divCardImg.setAttribute('class', 'card_grid_img')
+      img.setAttribute('loading', 'lazy')
+      img.setAttribute('src', arrayProduct.image)
+      img.setAttribute('alt', arrayProduct.name)
+      seller.classList.add('seller')
+      p.setAttribute('class', 'card_item_p')
+      a.setAttribute('href', arrayProduct.href)
+      a.setAttribute('loading', 'lazy')
+      a.setAttribute('class', 'text_special_card')
+      divDescriptionCard.setAttribute('class', 'description_card spacing_top')
+      btnCard.setAttribute('class', 'btn-buy')
 
-    //TEXT CONTENT
-    wordRecomended.textContent = arrayProduct.name
-    wordRecomended.addEventListener('click', () => {
-      input.value = arrayProduct.name
-    })
-    hthree.textContent = arrayProduct.name
-    pDescription.textContent = arrayProduct.price
-    spanDescription.textContent = 'Precio minimo historico'
-    seller.textContent = `${arrayProduct.seller}`
-    btnCard.textContent = 'Comprar'
-    a.textContent = 'Ofertas días azulados'
+      //TEXT CONTENT
+      wordRecomended.textContent = arrayProduct.name
+      wordRecomended.addEventListener('click', () => {
+        input.value = arrayProduct.name
+      })
+      hthree.textContent = arrayProduct.name
+      pDescription.textContent = arrayProduct.price
+      spanDescription.textContent = 'Precio minimo historico'
+      seller.textContent = `${arrayProduct.seller}`
+      btnCard.textContent = 'Comprar'
+      a.textContent = 'Ofertas días azulados'
 
-    //APPEND
-    divGrid.appendChild(divCard)
-    divCard.appendChild(divCardImg)
-    divCardImg.appendChild(img)
-    divCardImg.appendChild(p)
-    p.appendChild(b)
-    b.appendChild(a)
-    divCard.appendChild(divDescriptionCard)
-    divDescriptionCard.appendChild(hthree)
-    divDescriptionCard.appendChild(pDescription)
-    divDescriptionCard.appendChild(spanDescription)
-    divDescriptionCard.appendChild(seller)
-    divCard.appendChild(btnCard)
+      //APPEND
+      divGrid.appendChild(divCard)
+      divCard.appendChild(divCardImg)
+      divCardImg.appendChild(img)
+      divCardImg.appendChild(p)
+      p.appendChild(b)
+      b.appendChild(a)
+      divCard.appendChild(divDescriptionCard)
+      divDescriptionCard.appendChild(hthree)
+      divDescriptionCard.appendChild(pDescription)
+      divDescriptionCard.appendChild(spanDescription)
+      divDescriptionCard.appendChild(seller)
+      divCard.appendChild(btnCard)
+    }
   }
 }
